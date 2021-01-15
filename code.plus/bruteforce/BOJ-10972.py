047b28cb80a395002a511c623945868fcfaafbcd
leetaekyu2077 <dnjsqls2008@gmail.com>
2021-01-16 02:52:03 +0900

    bruteforce

---
+++ code.plus/bruteforce/BOJ-10972.py
@@ -0,0 +1,28 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+lst = list(map(int,sys.stdin.readline().split()))
+
+def get_next(lst):
+    if n == 1:
+        return [-1]
+    for i in range(n-1, 0, -1):
+        if lst[i] > lst[i-1]:
+            break
+    if i == 1 and lst[0] > lst[1]:
+        return [-1]
+    
+    for j in range(n-1,i-1,-1):
+        if lst[i-1] < lst[j]:
+            break
+    
+    lst[i-1],lst[j] = lst[j],lst[i-1]
+    lst = lst[0:i]+sorted(lst[i:])
+    return lst
+
+lst = get_next(lst)
+lst = [str(i) for i in lst]
+print(' '.join(lst))
+
+    

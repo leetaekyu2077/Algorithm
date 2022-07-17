9fd0fa643df910190231ada6452db365224f88a9
LeeTaeGyu <dnjsqls2008@gmail.com>
2022-07-18 03:13:49 +0900

    Add BOJ-10699 with Java

---
+++ BOJ/Implementation/BOJ-10699(오늘 날짜)/Main.java
@@ -0,0 +1,8 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(DateTimeFormatter.ofPattern("yyyy-MM-dd").format(LocalDateTime.now().plusHours(9)));
+    }
+}

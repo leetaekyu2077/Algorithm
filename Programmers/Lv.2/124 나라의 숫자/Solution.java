aa4386875a4d6fe6ebbb7f0d5883f4c0f5090436
LeeTaeGyu <dnjsqls2008@gmail.com>
2022-09-06 05:09:39 +0900

    Update Solution.java

--- Programmers/Lv.2/124 나라의 숫자/Solution.java
+++ Programmers/Lv.2/124 나라의 숫자/Solution.java
@@ -17,3 +17,19 @@ class Solution {
         return answer;
     }
 }
+
+// 채점 후 다른 사람 풀이 보고 수정한 코드 
+// (왜 answer를 저렇게 붙일 생각을 안 했을까)
+
+// class Solution {
+//     public String solution(int n) {
+//         String answer = "";
+//         String[] nums = {"4", "1", "2"};
+        
+//         while (n != 0) {
+//             answer = nums[n%3] + answer;
+//             n = (n-1) / 3;
+//         }
+//         return answer;
+//     }
+// }

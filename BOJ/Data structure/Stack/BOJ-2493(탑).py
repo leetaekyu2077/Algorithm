1174ec47ae96441c25a88e5420358381fc12a4c9
LeeTaeGyu <dnjsqls2008@gmail.com>
2023-01-11 01:52:29 +0900

    Update BOJ-2493(탑).py

--- BOJ/Data structure/Stack/BOJ-2493(탑).py
+++ BOJ/Data structure/Stack/BOJ-2493(탑).py
@@ -3,13 +3,12 @@ from sys import stdin
 
 N = int(stdin.readline())
 towers = list(map(int, stdin.readline().split()))
-stk = [(N-1, towers[N-1])]
+stk = []
 answers = ['0'] * N
 
-for i in range(N-2, -1, -1):
+for i in range(N-1, -1, -1):
     while stk and towers[i] >= stk[-1][1]:
-        temp = stk.pop()
-        answers[temp[0]] = str(i+1)
+        answers[stk.pop()[0]] = str(i+1)
     stk.append((i, towers[i]))
 
 print(" ".join(answers))

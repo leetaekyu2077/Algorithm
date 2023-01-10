1eab1b3be3ddb2a9205f55e891fed10146ec6d87
LeeTaeGyu <dnjsqls2008@gmail.com>
2023-01-11 01:46:43 +0900

    Add extra solution (순방향 탐색 방법)

--- BOJ/Data structure/Stack/BOJ-2493(탑).py
+++ BOJ/Data structure/Stack/BOJ-2493(탑).py
@@ -1,3 +1,4 @@
+# 뒤에서부터 역순으로
 from sys import stdin
 
 N = int(stdin.readline())
@@ -12,3 +13,23 @@ for i in range(N-2, -1, -1):
     stk.append((i, towers[i]))
 
 print(" ".join(answers))
+
+# 앞에서부터 차례대로
+# from sys import stdin
+
+# N = int(stdin.readline())
+# towers = list(map(int, stdin.readline().split()))
+# answers = ['0'] * N
+# stk = []
+
+# for i in range(N):
+#     while stk:
+#         if towers[i] > stk[-1][1]:
+#             stk.pop()
+#         else:
+#             answers[i] = str(stk[-1][0]+1)
+#             break
+        
+#     stk.append((i, towers[i]))
+
+# print(" ".join(answers))

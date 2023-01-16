0ce3d9ea7a873faf86fec417256a824507ca431e
LeeTaeGyu <dnjsqls2008@gmail.com>
2023-01-17 00:57:52 +0900

    Add BOJ-11437 extra solution (순위권 풀이 참조)

--- BOJ/Data structure/Tree/BOJ-11437(LCA).py
+++ BOJ/Data structure/Tree/BOJ-11437(LCA).py
@@ -2,6 +2,7 @@ import sys, math
 input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
 
+# 1. LCA 알고리즘 사용 풀이
 def makeTree(node, dep):
     depth[node] = dep
     for i in range(1, max):
@@ -13,6 +14,12 @@ def makeTree(node, dep):
             makeTree(c, dep+1)
         
 def lca(a, b):
+    if a > b:
+        x, y = b, a
+    
+    if (x, y) in answers:
+        return answers[(x, y)]
+    
     if depth[a] != depth[b]:
         if depth[a] > depth[b]:
             a, b = b, a       
@@ -27,6 +34,8 @@ def lca(a, b):
                 a = parents[a][i]
                 b = parents[b][i]
             lca = parents[a][i]  
+            
+    answers[(x, y)] = lca
     return lca
 
 n = int(input())
@@ -35,6 +44,7 @@ depth = [0]*(n+1)
 max = int(math.log2(n-1))+1
 parents = [[0]*max for _ in range(n+1)]
 child = [[] for _ in range(n+1)]
+answers = {}
 
 for _ in range(n-1):
     a, b = map(int, input().split())
@@ -48,3 +58,54 @@ m = int(input())
 for _ in range(m):
     a, b = map(int, input().split()) 
     print(lca(a, b))
+
+# ----------------------------------------------------------
+# 2. 테스트 케이스 결과 dictionary에 저장 (일종의 DP?)
+# def makeTree(node, dep):
+#     depth[node] = dep
+#     for c in child[node]:
+#         if parents[c] == 0:
+#             parents[c] = node
+#             makeTree(c, dep+1)
+            
+# def lca(a, b):
+#     x, y = a, b    
+#     if a > b: 
+#         x, y = b, a
+        
+#     if (x, y) in answers:
+#         return answers[(x, y)]
+    
+#     if depth[a] > depth[b]:
+#         a, b = b, a
+    
+#     if (a, b) in answers:
+#         return answers[(a, b)]
+    
+#     while depth[a] != depth[b]:
+#         b = parents[b]
+#     while a != b:
+#         a = parents[a]
+#         b = parents[b]   
+    
+#     answers[(x, y)] = a
+#     return a      
+
+# n = int(input())
+# depth = [0]*(n+1)
+# parents = [0]*(n+1)
+# child = [[] for _ in range(n+1)]
+# answers = {}
+
+# for _ in range(n-1):
+#     a, b = map(int, input().split())
+#     child[a].append(b) 
+#     child[b].append(a)
+    
+# parents[1] = 1
+# makeTree(1, 1)  
+
+# m = int(input())
+# for _ in range(m):
+#     a, b = map(int, input().split()) 
+#     print(lca(a, b))

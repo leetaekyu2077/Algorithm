e4ab05c29d7ebead614c0cc1e1123004d84307a0
LeeTaeGyu <dnjsqls2008@gmail.com>
2023-03-20 20:43:52 +0900

    Update BOJ-1463(1로 만들기).py

--- BOJ/DP/BOJ-1463(1로 만들기).py
+++ BOJ/DP/BOJ-1463(1로 만들기).py
@@ -1,16 +1,26 @@
 import sys
 input = sys.stdin.readline
 
+records = dict()
+records[1] = 0
+records[2] = 1
+
+def sol(num):
+    if num in records:
+        return records[num]
+    else:
+        records[num] = min(sol(num//3)+num%3, sol(num//2)+num%2)+1
+        return records[num]
+
 n = int(input())
-dp = [0]*(n+1)
 
-for i in range(1, n+1):
-    nums = [dp[i-1]]    
-    if i%3 == 0:
-        nums.append(dp[i//3])
-    if i%2 == 0:
-        nums.append(dp[i//2])
+# for i in range(1, n+1):
+#     nums = [dp[i-1]]    
+#     if i%3 == 0:
+#         nums.append(dp[i//3])
+#     if i%2 == 0:
+#         nums.append(dp[i//2])
         
-    dp[i] = min(nums)+1
+#     dp[i] = min(nums)+1
 
-print(dp[n]-1)
+print(sol(n))

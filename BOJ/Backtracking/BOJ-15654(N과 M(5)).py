9109759b485e0da35045c3fb187447d08da08267
LeeTaeGyu <dnjsqls2008@gmail.com>
2023-09-05 14:36:10 +0900

    Add: resolved BOJ-15654 with python

--- BOJ/Backtracking/BOJ-15654(N과 M(5)).py
+++ BOJ/Backtracking/BOJ-15654(N과 M(5)).py
@@ -1,17 +1,34 @@
+# import sys
+
+# n, m = map(int, sys.stdin.readline().split())
+# nums = sorted(list(map(int, sys.stdin.readline().split())))
+
+
+# def dfs(comb, length):
+#     if length == m:
+#         print(' '.join(map(str, comb)))
+#         return 
+#     else:
+#         for num in nums:
+#             if num not in comb:
+#                 comb.append(num)
+#                 dfs(comb, length+1)
+#                 comb.pop()          
+# dfs([], 0)
+
 import sys
+input = sys.stdin.readline
 
-n, m = map(int, sys.stdin.readline().split())
-nums = sorted(list(map(int, sys.stdin.readline().split())))
+n, m = map(int, input().split())
+nums = sorted(list(map(int, input().split())))
 
+def back_tracking(case):
+    if len(case) == m:
+        print(*case, sep=' ')
+        return
 
-def dfs(comb, length):
-    if length == m:
-        print(' '.join(map(str, comb)))
-        return 
-    else:
-        for num in nums:
-            if num not in comb:
-                comb.append(num)
-                dfs(comb, length+1)
-                comb.pop()          
-dfs([], 0)
+    for n in nums:
+        if n not in case:
+            back_tracking(case+[n])
+
+back_tracking([])

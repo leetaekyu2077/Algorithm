cc458cefd4c3a0b1b1341f302dcebf4163428428
LeeTaeGyu <dnjsqls2008@gmail.com>
2023-09-05 14:20:01 +0900

    Add: resolved BOJ-15651 with python

--- BOJ/Backtracking/BOJ-15651(N과 M(3)).py
+++ BOJ/Backtracking/BOJ-15651(N과 M(3)).py
@@ -1,16 +1,31 @@
+# import sys
+
+# n, m = map(int, sys.stdin.readline().split())
+
+# def dfs(comb, length):
+#     if length == m:
+#         print(' '.join(map(str, comb)))
+#         return 
+#     else:
+#         num = 1
+#         while num < n+1:
+#             comb.append(num)
+#             dfs(comb, length+1)
+#             comb.pop()
+#             num += 1           
+# dfs([], 0)
+
 import sys
+input = sys.stdin.readline
 
-n, m = map(int, sys.stdin.readline().split())
+n, m = map(int, input().split())
 
-def dfs(comb, length):
-    if length == m:
-        print(' '.join(map(str, comb)))
-        return 
-    else:
-        num = 1
-        while num < n+1:
-            comb.append(num)
-            dfs(comb, length+1)
-            comb.pop()
-            num += 1           
-dfs([], 0)
+def back_tracking(case):
+    if len(case) == m:
+        print(' '.join(case))
+        return
+
+    for i in range(1, n+1):
+        back_tracking(case+[str(i)])
+
+back_tracking([])

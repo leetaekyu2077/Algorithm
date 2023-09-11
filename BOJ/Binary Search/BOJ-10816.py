ad1ef2bbe8d89e45d2617f0fec52a5e1b42e2a30
LeeTaeGyu <dnjsqls2008@gmail.com>
2023-09-11 15:01:13 +0900

    Add: solved BOJ-10816 with python

---
+++ BOJ/Binary Search/BOJ-10816.py
@@ -0,0 +1,11 @@
+import sys
+from bisect import bisect_left, bisect_right
+input = sys.stdin.readline
+
+n = int(input())
+cards = sorted(list(map(int, input().split())))
+m = int(input())
+targets = list(map(int, input().split()))
+
+for t in targets:
+    print(bisect_right(cards, t)-bisect_left(cards, t), end=' ')
